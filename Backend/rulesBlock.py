5fc651db3d171eb98d8b25a789dcfd850e71e354
Mathieu <Mathieu@DESKTOP-354848G>
2019-06-24 10:22:32 +0200

    algorithm rulesBlock added

---
+++ Backend/rulesBlock.py
@@ -0,0 +1,11 @@
+from Backend import CellArray
+
+def UniqueCellColor(CellArray, searchedCellValue, wantedCellValue):
+    for value in CellArray:
+        if value == searchedCellValue:
+            value = wantedCellValue
+
+def OneNeighbourCellColor(CellArray, searchedCellValue, wantedCellValue):
+    for value in CellArray:
+        if ()
+
